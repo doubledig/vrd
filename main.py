59193b0b92c493c64d0d20b515620506740352f3
jinbi <jinbicheng029@outlook.com>
2023-03-18 00:31:05 +0800

    完成了f_rcnn的搭建

--- main.py
+++ main.py
@@ -1,4 +1,67 @@
+import json
+import logging
+import time
 
+import torch
+import torchvision.models.detection
+from torch.utils.data import DataLoader
 
+import mydata
+
+# 思源宋体
 if __name__ == '__main__':
+    # 相关设置
+    if torch.cuda.is_available():
+        device = 'cuda'
+    else:
+        device = 'cpu'
+    train_rcnn = True
+    epoch = 100
+    # 加载数据集
+    dataset = mydata.myDataset('data/VRD', 'train', train_rcnn, device=device)
+    dataloader = DataLoader(dataset,
+                            shuffle= True)
+    # 创建日志
+    logger = logging.getLogger('logger')
+    sh = logging.StreamHandler()
+    fh = logging.FileHandler('log/{}_log.txt'.format(time.strftime("%Y_%m_%d_%H_%M_%S", time.localtime())),
+                             encoding='utf-8')
+    logger.addHandler(sh)
+    logger.addHandler(fh)
+    lf = logging.Formatter(fmt='%(asctime)s | %(levelname)s | %(message)s',
+                           datefmt='%Y_%m_%d %H:%M:%S')
+    sh.setFormatter(lf)
+    fh.setFormatter(lf)
+    logger.setLevel(logging.INFO)
+    sh.setLevel(logging.INFO)
+    fh.setLevel(logging.INFO)
+    if train_rcnn:
+        # 训练用于目标检测的网络并输出参数保存
+        rcnn_module = torchvision.models.detection.fasterrcnn_resnet50_fpn_v2(num_classes=101,
+                                                                              weights_backbone="IMAGENET1K_V2")
+        rcnn_module.train()
+        rcnn_module.to(device=device)
+        optimizer = torch.optim.Adam(rcnn_module.parameters(), 1e-3)
+        t0 = time.time()
+        for i in range(10):
+            for iter_id, (images, targets) in enumerate(dataloader):
+                losses = rcnn_module(images, targets)
+                all_loss = 0
+                optimizer.zero_grad()
+                for loss in losses:
+                    all_loss += losses[loss]
+                all_loss.backward()
+                optimizer.step()
+                if iter_id % 100 == 0:
+                    t1 = time.time() - t0
+                    logger.info('[Epoch {:d}] [Iter {:d}/4000] '
+                                '[Loss: loss_classifier {:.2f} || loss_box_reg {:.2f} '
+                                '|| loss_objectness {:.2f} || loss_rpn_box_reg {:.2f} || total {:.2f} ]'
+                                '[time: {:.2f}]'.format(i, iter_id, losses['loss_classifier'], losses['loss_box_reg'],
+                                                        losses['loss_objectness'], losses['loss_rpn_box_reg']
+                                                        , all_loss, t1))
+                    t0 = time.time()
+        # 保存模型到本地
+        torch.save({'rcnn_module': rcnn_module,
+                    'optimizer': optimizer}, 'module/frcnn.pth')
     pass
--- mydata.py
+++ mydata.py
@@ -3,18 +3,24 @@ import os
 from typing import Any, Tuple
 
 import torch.utils.data
-from PIL import Image
+import torchvision.io
+from torchvision.models.detection import FasterRCNN_ResNet50_FPN_V2_Weights
 
 
 class myDataset(torch.utils.data.Dataset):
 
     def __init__(self, root: str,
                  split: str,
-                 t_rcnn: bool = False) -> None:
+                 t_rcnn: bool = False,
+                 device = None) -> None:
         self.split = split
         self.t_rcnn = t_rcnn
         root = os.path.expanduser(root)
         self.root = root
+        if device is None:
+            self.device = 'cpu'
+        else:
+            self.device = device
         with open(os.path.join(self.root, 'objects.json')) as f:
             self.objects = json.load(f)
         with open(os.path.join(self.root, 'predicates.json')) as f:
@@ -25,14 +31,35 @@ class myDataset(torch.utils.data.Dataset):
 
     def __getitem__(self, index: int) -> Tuple[Any, Any]:
         path = os.path.join(self.root, self.split, self.ids[index])
-        image = Image.open(path).convert("RGB")
+        image = torchvision.io.read_image(path, mode=torchvision.io.ImageReadMode.RGB).to(self.device)
         target = []
         if self.t_rcnn:
             for li in self.ann[self.ids[index]]:
                 for k in ['object', 'subject']:
-                    h = li[k]['bbox'].append(li[k]['category'])
-                    if h not in target:
+                    h = {'boxes': torch.tensor([li[k]['bbox'][2], li[k]['bbox'][0], li[k]['bbox'][3], li[k]['bbox'][1]],
+                                               device=self.device,
+                                               dtype=torch.float),
+                         'labels': torch.tensor(li[k]['category'] + 1, device=self.device)}
+                    t_h = True
+                    # 检测重复物体
+                    for t in target:
+                        if h['boxes'].equal(t['boxes']) and h['labels'].equal(t['labels']):
+                            t_h = False
+                            break
+                    if t_h:
                         target.append(h)
+            # 处理无标签情况
+            if len(target) == 0:
+                h = {'boxes': torch.tensor([0, 0, image.size(1), image.size(2)],
+                                           device=self.device,
+                                           dtype=torch.float),
+                     'labels': torch.tensor(0, device=self.device)}
+                target.append(h)
         else:
             pass
+        preprocess = FasterRCNN_ResNet50_FPN_V2_Weights.COCO_V1.transforms()
+        image = preprocess(image)
         return image, target
+
+    def __len__(self):
+        return len(self.ids)
